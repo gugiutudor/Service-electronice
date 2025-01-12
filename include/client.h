03fcc9498dccf6a73c253ef5162f684c22cad5ab
gugiutudor <gugiutudor@gmail.com>
2025-01-12 22:41:07 +0200

    Add files via upload

---
+++ include/client.h
@@ -0,0 +1,28 @@
+#ifndef CLIENT_H_INCLUDED
+#define CLIENT_H_INCLUDED
+
+#include <vector>
+#include "dispozitiv.h"
+
+class Client
+{
+private:
+    int id;
+    std::string nume;
+    std::string telefon;
+    std::vector<Dispozitiv*> dispozitive;
+    static int idUrmator;
+
+public:
+    Client(const std::string& nume, const std::string telefon);
+
+    void adaugaDispozitiv(Dispozitiv* d);
+
+    friend std::ostream& operator<<(std::ostream& out, const Client& c);
+
+    int getId() const;
+    std::string getNume() const;
+    const std::vector<Dispozitiv*>& getDispozitive() const;
+};
+
+#endif // CLIENT_H_INCLUDED
