c71358ff7af5bea17a7242486032c660f4af45a8
gugiutudor <gugiutudor@gmail.com>
2025-01-12 22:06:23 +0200

    Add files via upload

---
+++ src/dispozitiv.cpp
@@ -0,0 +1,99 @@
+#include "Dispozitiv.h"
+
+int Dispozitiv::idUrmator = 0;
+
+Dispozitiv::Dispozitiv():
+    id(++idUrmator), marca(""), model(""), pret(0), inGarantie(false) {}
+
+Dispozitiv::Dispozitiv(const std::string& marca, const std::string& model, int pret, bool inGarantie):
+    id(++idUrmator), marca(marca), model(model), pret(pret), inGarantie(inGarantie) {}
+
+Dispozitiv::Dispozitiv(const Dispozitiv& other):
+    id(other.id), marca(other.marca), model(other.model), pret(other.pret), inGarantie(other.inGarantie) {}
+
+Dispozitiv& Dispozitiv::operator=(const Dispozitiv& other)
+{
+    if (this!=&other)
+    {
+        id=other.id;
+        marca=other.marca;
+        model=other.model;
+        pret=other.pret;
+        inGarantie=other.inGarantie;
+    }
+    return *this;
+}
+
+std::ostream& operator<<(std::ostream& out, const Dispozitiv& d)
+{
+    out<<"\nID: "<<d.id
+       <<"\nMarca: "<<d.marca
+       <<"\nModel: "<<d.model
+       <<"\nPret: "<<d.pret<<" RON"
+       <<"\nGarantie: "<<(d.inGarantie?"Da":"Nu")<<"\n";
+    return out;
+}
+
+std::istream& operator>>(std::istream& in, Dispozitiv& d)
+{
+    std::cout<<"Marca: ";
+    in.ignore();
+    std::getline(in, d.marca);
+    std::cout<<"Model: ";
+    std::getline(in, d.model);
+    std::cout<<"Pret: ";
+    in>>d.pret;
+    std::cout<<"In garantie (Introduceti 1 pentru 'da'/Introduceti 0 pentru 'nu'): ";
+    in>>d.inGarantie;
+    in.ignore();
+    return in;
+}
+
+Dispozitiv& Dispozitiv::operator+(float valoare)
+{
+    this->pret+=valoare;
+    return *this;
+}
+
+Dispozitiv& operator*(Dispozitiv& d, int procent)
+{
+    d.pret=d.pret+(d.pret*procent/100.0f);
+    return d;
+}
+
+int Dispozitiv::getId() const
+{
+    return id;
+}
+
+std::string Dispozitiv::getMarca() const
+{
+    return marca;
+}
+
+std::string Dispozitiv::getModel() const
+{
+    return model;
+}
+
+float Dispozitiv::getPret() const
+{
+    return pret;
+}
+
+bool Dispozitiv::getInGarantie() const
+{
+    return inGarantie;
+}
+
+void Dispozitiv::setPret(float pretNou)
+{
+    pret=pretNou;
+}
+
+void Dispozitiv::setGarantie(bool garantieNoua)
+{
+    inGarantie=garantieNoua;
+}
+
+Dispozitiv::~Dispozitiv() {}
---
+++ src/pc.cpp
@@ -0,0 +1,88 @@
+#include "PC.h"
+
+PC::PC(): Dispozitiv(), tipProcesor(""), frecventaProcesor(0),
+    memorie(0), tipPlacaVideo(""), capacitatePlacaVideo(0) {}
+
+PC::PC(const std::string& marca, const std::string& model, int pret, bool inGarantie,
+       const std::string& tipProcesor, float frecventaProcesor, int memorie,
+       const std::string& tipPlacaVideo, int capacitatePlacaVideo):
+    Dispozitiv(marca, model, pret, inGarantie), tipProcesor(tipProcesor), frecventaProcesor(frecventaProcesor),
+    memorie(memorie), tipPlacaVideo(tipPlacaVideo), capacitatePlacaVideo(capacitatePlacaVideo) {}
+
+PC::PC(const PC& other):
+    Dispozitiv(other), tipProcesor(other.tipProcesor), frecventaProcesor(other.frecventaProcesor), memorie(other.memorie),
+    tipPlacaVideo(other.tipPlacaVideo), capacitatePlacaVideo(other.capacitatePlacaVideo) {}
+
+PC& PC::operator=(const PC& other)
+{
+    if (this!=&other)
+    {
+        Dispozitiv::operator=(other);
+        tipProcesor=other.tipProcesor;
+        frecventaProcesor=other.frecventaProcesor;
+        memorie=other.memorie;
+        tipPlacaVideo=other.tipPlacaVideo;
+        capacitatePlacaVideo=other.capacitatePlacaVideo;
+    }
+    return *this;
+}
+
+std::istream& operator>>(std::istream& in, PC& p)
+{
+    in>>static_cast<Dispozitiv&>(p);
+    std::cout<<"Tip procesor: ";
+    std::getline(in, p.tipProcesor);
+    std::cout<<"Frecventa procesor (GHz): ";
+    in>>p.frecventaProcesor;
+    std::cout<<"Memorie RAM (GB): ";
+    in>>p.memorie;
+    in.ignore();
+    std::cout<<"Tip placa video: ";
+    std::getline(in, p.tipPlacaVideo);
+    std::cout<<"Capacitate placa video (GB): ";
+    in>>p.capacitatePlacaVideo;
+    in.ignore();
+    return in;
+}
+
+void PC::diagnosticare()
+{
+    verificaMemorie();
+    verificaPlacaVideo();
+}
+
+float PC::calculeazaCostReparatie()
+{
+    float cost=0;
+    if (memorie<16)
+        cost+=400;
+    if (capacitatePlacaVideo<8)
+        cost+=2000;
+    return cost;
+}
+
+void PC::afiseazaDetaliiSpecifice()
+{
+    std::cout<<"Detalii PC:\n";
+    std::cout<<"Procesor: "<<tipProcesor<<" "<<frecventaProcesor<<"GHz\n";
+    std::cout<<"Memorie RAM: "<<memorie<<"GB\n";
+    std::cout<<"Placa video: "<<tipPlacaVideo<<" "<<capacitatePlacaVideo<<"GB\n";
+}
+
+void PC::verificaMemorie()
+{
+    std::cout<<"Test memorie RAM: "<<memorie<<"GB\n";
+    if (memorie<16)
+        std::cout<<"RAM insuficient - se recomanda upgrade la minim 16GB.\n";
+    else
+        std::cout<<"Memorie RAM suficienta pentru utilizare normala.\n";
+}
+
+void PC::verificaPlacaVideo()
+{
+    std::cout<<"Verificare placa video: "<<tipPlacaVideo<<" "<<capacitatePlacaVideo<<"GB\n";
+    if (capacitatePlacaVideo<8)
+        std::cout<<"Se recomanda upgrade la o placa video cu minim 8GB VRAM.\n";
+    else
+        std::cout<<"Placa video corespunde cerintelor actuale.\n";
+}
