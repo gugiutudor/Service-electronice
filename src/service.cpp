1a2f0bd8e72148be68536319b3128ab354ff173c
gugiutudor <gugiutudor@gmail.com>
2025-01-12 22:42:23 +0200

    Add files via upload

---
+++ src/service.cpp
@@ -0,0 +1,26 @@
+#include "service.h"
+
+Service::Service(const std::string& nume): nume(nume) {}
+
+void Service::adaugaClient(Client* client)
+{
+    clienti.push_back(client);
+}
+
+void Service::primesteDispozitiv(Dispozitiv* dispozitiv, int idClient)
+{
+    for(Client* client: clienti)
+        if(client->getId()==idClient)
+            for(Dispozitiv* d: client->getDispozitive())
+                if(d==dispozitiv)
+                    dispozitiveActive.push_back(dispozitiv);
+}
+
+std::ostream& operator<<(std::ostream& out, const Service& s)
+{
+    out<<"\nService "<<s.nume;
+    out<<"\nDispozitive in stadiu de reparare:";
+    for(Dispozitiv* d: s.dispozitiveActive)
+        out<<"\n"<<*d;
+    return out;
+}
